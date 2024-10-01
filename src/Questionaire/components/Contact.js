65628aa18048bbe2c2f87677e4cfc8c3aa9a739b
Pascale George <pascalegeorge@Pascales-MacBook-Pro-2.local>
2024-10-01 16:31:48 +0300

    renamings

--- src/Questionaire/components/Contact.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import '../styles/contact.css';
-export default function Contact({ heading, helper, contacts, }) {
-    return (_jsxs("div", { className: "contact-container", children: [_jsxs("div", { className: "heading-container", children: [_jsx("h2", { children: heading }), helper && (_jsx("p", { className: "helper", children: { helper } }))] }), _jsx("div", { className: "contact-methods main-content-container", children: contacts.map((method) => {
-                    return (_jsxs("div", { className: "contact", "data-option": method.text, children: [_jsx("input", { type: "radio", name: "contact_method", value: method.text.replace('Ș', 'S').replace('ț', 't').replace('ă', 'a'), className: "hs_contact hidden" }), _jsx("img", { src: method.image_url, alt: "Contact Folk", width: "100%", height: "auto" }), _jsx("div", { className: "contact-text", children: method.text }), _jsx("div", { className: "contact-description", children: method.description })] }, method.text));
-                }) })] }));
-}
--- src/Questionaire/components/HeaderArea.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-export default function HeaderArea({ currentPage, progressPages, qLength }) {
-    return (_jsxs(_Fragment, { children: [_jsx("a", { href: "/", children: _jsx("img", { src: "https://cdn.shopify.com/s/files/1/0761/6112/2639/files/folk_logo.svg?v=1689765174", alt: "Folk", className: "logo" }) }), currentPage.id !== 1 && (_jsx("div", { className: "progress-bar-container", id: "progress-bar-container", children: _jsx("div", { className: "progress-bar", children: _jsx("span", { id: "progress", style: { width: (progressPages.length * 100 / qLength) + '%' } }) }) }))] }));
-}
