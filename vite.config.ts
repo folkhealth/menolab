ccbc139d5e7447b5a41fe236cb2f98229978f775
Pascale George <pascalegeorge@Pascales-MacBook-Pro-2.local>
2024-10-10 17:18:53 +0300

    remove proxy server

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://vl23sex5f0.execute-api.eu-north-1.amazonaws.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://vl23sex5f0.execute-api.eu-north-1.amazonaws.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     }
+  //   }
+  // }
 })
