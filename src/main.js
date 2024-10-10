c1d41fe1b48f14a883e168b25ed61d3051f724cf
Pascale George <pascalegeorge@Pascales-MacBook-Pro-2.local>
2024-10-10 17:06:22 +0300

    updates jsx linting and buil command

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-Amplify.configure(outputs);
-ReactDOM.createRoot(document.getElementById("root")).render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
